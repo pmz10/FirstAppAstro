90083b832c0c9d86571dc871379cd0145c856cd7
GeraDes10 <pmz11_garcia@hotmail.com>
2023-07-14 12:36:31 -0600

    AstroApp

---
+++ src/Data/books.js
@@ -0,0 +1,30 @@
+export const books = [
+    {
+        id: 'clear-code',
+        title: 'Clear Code: Manual de desarrollo ágil de software',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+        author: "Control Machete",
+        opinion: "Tendre que esperar más volumnes por que se queda corto"
+    },
+    {
+        id: 'test-drive',
+        title: 'Test-Driven Development Kent Beck',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1372039943i/387190.jpg",
+        author: "Control Machete",
+        opinion: "Me agrado el contenido y la manera en la que lo despliega",
+    },
+    {
+        id: 'eloquent-javascript',
+        title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+        author: "Control Machete",
+        opinion: "Tiene informacioón curiosa que una vez que verificas es fantastica"
+    },
+    {
+        id: 'javascript-TheDefinitive',
+        title: 'JavaScript: The Definitive Guide David Flanagan',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347652925i/148050.jpg",
+        author: "Control Machete",
+        opinion: "Ha estado bien"
+    },
+]
